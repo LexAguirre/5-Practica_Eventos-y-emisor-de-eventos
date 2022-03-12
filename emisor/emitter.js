fa347d54d62d660183233eec48295a33104deaa0
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2022-03-12 11:14:55 -0600

    Implemented events/app and started emitter

--- emisor/emitter.js
+++ emisor/emitter.js
@@ -1,6 +1,7 @@
 function Emitter() {
+  //Esta es la funcion constructora y dentro de esta se inicializa los contenedores de informacion donde se guarda la informacion
   this.events = {};
-}
+} //Este modulo exporta el constructor
 
 Emitter.prototype.on = function (type, listener) {
   this.events[type] = this.events[type] || [];
--- events/app.js
+++ events/app.js
@@ -1,7 +1,13 @@
 let obj = {
   nombre: "Ramon",
-  edad: 44,
+  edad: 30,
   apellido: "Aguirre",
+  keypress: function () {
+    console.log("Se ha precionado una tecla");
+  },
+  mouseOver: function () {
+    console.log("El puntero del mouse esta arriba");
+  },
 };
 
 console.log(obj.nombre); //Esta manera de extraer informacion de un objeto es la vista en la practica anterior
@@ -11,10 +17,11 @@ let mykey = "nombre"; // Esta es la variable que se ingresara en los []
 console.log(obj[mykey]); //ahora se remplazara la variable que contiene el nombre del elemento
 
 mykey = "keypress";
-console.log(obj[mykey]()); //
+console.log(obj[mykey]());
 
 mykey = "mouseOver";
-console.log(obj[mykey]()); //
+console.log(obj[mykey]());
+//En las dos como la funcion no regresa nada sino imprime por su cuente el string entonces al mandar llamar las funciones me imprime lo que tiene la funcion pero tambien un indefinido
 
 let myArray = [];
 
@@ -22,13 +29,20 @@ myArray.push(4);
 myArray.push("una cadena");
 myArray.push(obj);
 myArray.push(function () {
-  Console.log("Hola desde el array");
+  console.log("Hola desde el array");
 });
 console.log(myArray);
 
-let funcArray = [];
+//Imprime todo el contenido del array sin cambiar el tipo de ninguno de los datos ya que imprime el numero, string, objeto y la funcion que se encuentran en el array
+
+console.log(myArray[2][mykey]()); // En esta linea se imprime un dato del objeto al mandar llamar primero el objeto y luego colocar el parametro con el que buscara el dato
+
+console.log(myArray[3]()); // En esta linea se manda llamar el objeto y luego se le colocan los parentesis para que se ejecuta como una función
+
+let funcArray = []; // Se creara un array el cual contenda solamente funciones
 
 funcArray.push(() => {
+  // Con push empujamos la funcion al nuevo array
   console.log("Function 1");
 });
 
@@ -42,4 +56,4 @@ funcArray.push(() => {
 
 funcArray.forEach((item) => {
   item();
-});
+}); //Con forEach recorreremos todos los elementos del array colocandolo en el item y se le colocaran los parentesis para que se ejecute como una función
