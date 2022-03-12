9d0d6784a8df71c6239738311421dc40614d1b86
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2022-03-12 12:17:41 -0600

    Implemented emitter and app in emisor

--- emisor/app.js
+++ emisor/app.js
@@ -1,21 +1,24 @@
-var Emitter = require("./emitter");
+var Emitter = require("./emitter"); // Se manda llamar el modulo del archivo emitter.js
 
-var emtr = new Emitter();
+var emtr = new Emitter(); //Ahora se crea un evento con el modulo anterior
 
 emtr.on("greet", () => {
+  //ejecutamos el emtr.on para ingresar un array de greet con una string
   console.log("Somewhere, someone said hello.");
 });
 
 emtr.on("greet", () => {
+  //Ejecutamon el emtr.on para ingresar otro sting en greet
   console.log("A greeting occurred!");
 });
 
 console.log("Hello!");
-emtr.emit("greet");
+emtr.emit("greet"); //ejecutamos todo lo que tenga greet en el Emitter
 
 emtr.on("jump", () => {
+  //ahora ingresamos jump y un texto
   console.log("someone jumped!");
 });
 
-console.log(emtr);
-emtr.emit("jump");
+console.log(emtr); //ejecutamos todo el emtr
+emtr.emit("jump"); //ejecutamos el jump del Emitter
--- emisor/emitter.js
+++ emisor/emitter.js
@@ -4,16 +4,21 @@ function Emitter() {
 } //Este modulo exporta el constructor
 
 Emitter.prototype.on = function (type, listener) {
-  this.events[type] = this.events[type] || [];
-  this.events[type].push(listener);
+  //Con el metodo prototipo se podra estar aplicando la herencia
+  //Cuando se mande llamar a Emitter.on se ejecutara una funcion la cual ocupara type y listener
+  this.events[type] = this.events[type] || []; //Se revisa si hay algo dento de events si contiene algo se toma el contenido, pero si no entonces se le asigna un array vacio el cual sera llenado de funciones
+  this.events[type].push(listener); // Teniendo el array empujaremos el contenido en el elemento listener
 };
 
 Emitter.prototype.emit = function (type) {
+  //Esta funcion permitira emitir cualquier cual quier funcion que se le solicite buscar en el events
   if (this.events[type]) {
+    //Revisa si la funcion soliciada se encunentra
     this.events[type].forEach(function (listener) {
-      listener();
+      //si es encontrada almenos una entonces hace el recorrido para ver si hay otras con el mismo nombre
+      listener(); //las ejecuta
     });
-  }
+  } //Si no encuentra entonces cierra el if
 };
 
-module.exports = Emitter;
+module.exports = Emitter; //Exporta el modulo
